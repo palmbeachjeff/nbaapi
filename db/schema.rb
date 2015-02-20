7e3135f2d29989a61a35a211759a910bb8f93907
Prelang Builder <builder@prelang.com>
2015-02-20 12:50:32 -0500

    Executed 'rake db:migrate' (development and test) to migrate all generated migrations which created schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150220174614) do
+ActiveRecord::Schema.define(version: 20150220174633) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -45,6 +45,24 @@ ActiveRecord::Schema.define(version: 20150220174614) do
 
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+
+  create_table "predictions", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "game_id"
+    t.integer  "home_score"
+    t.integer  "away_score"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "teams", force: true do |t|
+    t.text     "name"
+    t.integer  "wins"
+    t.integer  "losses"
+    t.integer  "ties"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
